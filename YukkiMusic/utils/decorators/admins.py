ba6f45741367fbad9a984a167e54b13ad1b2e288
cnyox <abhichoudhary0970@gmail.com>
2025-07-27 13:25:31 +0530

    Minor Changes

--- YukkiMusic/utils/decorators/admins.py
+++ YukkiMusic/utils/decorators/admins.py
@@ -118,7 +118,7 @@ def AdminActual(mystic):
                 )
             except:
                 return
-            if not member.privileges.can_manage_voice_chats:
+            if not member.privileges.can_manage_video_chats:
                 return await message.reply(_["general_5"])
         return await mystic(client, message, _)
 
@@ -153,7 +153,7 @@ def ActualAdminCB(mystic):
                 return await CallbackQuery.answer(
                     _["general_5"], show_alert=True
                 )
-            if not a.privileges.can_manage_voice_chats:
+            if not a.privileges.can_manage_video_chats:
                 if CallbackQuery.from_user.id not in SUDOERS:
                     token = await int_to_alpha(
                         CallbackQuery.from_user.id
